3f90ea4ad8cc321f8778ebfb523bcb1e6ca1bd80
Pat Collins <padjo@mac.com>
2018-04-13 17:58:21 +0200

    added bootstrap4 styling to bookmarks app

--- djorg/settings.py
+++ djorg/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # third party apps
+    'bootstrap4',
     # our apps
     'bookmarks',
 ]
