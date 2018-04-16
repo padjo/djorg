6286903d26abe17f234231b8f3f3e405cdd3e177
Pat Collins <padjo@mac.com>
2018-04-17 00:45:49 +0200

    notes models completed

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+from uuid import uuid4
+from django.contrib.auth.models import User
+from django.db import models
+
+class Note(models.Model):
+    id = models.UUIDField(primary_key, default=uuid4, eidtable=False)
+    title = models.CharField(max_length=200)
+    content = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+
+    @property
+    def content_length(self):
+        return len(content )
